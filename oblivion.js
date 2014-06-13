0f63a1a18aefa2ebffac4c24d9c176690f8eb736
Pranay <spvenka@wal-mart.com>
2014-06-13 20:09:23 +0530

    Working demo.

--- oblivion.js
+++ oblivion.js
@@ -1,21 +1,25 @@
-
-var toSearch = "this snippet defines a function taking two integers";
 var counter = 1;
+var nodes = [];
+var searchStr = "Buy"; 
 function visitNodes(o) {
-	counter++;
-	if (o.childNodes.length == 0) {		
-		if (o.textContent && o.textContent.indexOf("I just found that it's the") >= 0) {
-			console.log("EUREKA!!!! Found it.");
-			o.textContent = "EUREKA EUREKA EUREKA!!!";
-		} 		
-		return true;
-	}
-
-	for(var i = 0; i < o.childNodes.length; i++) {
-		visitNodes(o.childNodes[i]);
+    counter = counter + 1;
+    if (o.childNodes.length == 0) {
+	if (o.textContent && o.textContent.indexOf(searchStr) >= 0){
+	    nodes.push(o.parentNode);
 	}
 	return true;
+    }
+
+    for(var i = 0; i < o.childNodes.length; i++) {
+	visitNodes(o.childNodes[i]);
+    }
+    return true;
 }
 
 visitNodes(document);
 counter
+    for (var i = 0; i < nodes.length; i++) {
+	var curnode = nodes[i];
+	curnode.innerHTML = curnode.innerHTML.replace(searchStr, '<a href="http://www.walmart.com">Walmart Home Page - Pranay</a>');
+    }
+nodes.length
